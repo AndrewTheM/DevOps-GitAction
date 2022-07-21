c0c7cd7312d929c86c13d1d0edbe5fa86b7cf769
Andrew Makoviichuk <42779167+AndrewTheM@users.noreply.github.com>
2022-07-21 13:11:13 +0300

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Variant 17
+base="Makoviichuk"
+content="2022 13.05.2022 andrewmakoviichuk@gmail.com"
+mkdir -p $base/VAR{H,K/GR{1,2},L}
+echo "$content" > $base/VARK/GR1/Andrii.txt
+echo "$content" > $base/VARK/GR2/Makoviichuk.txt
